9a2cccdcca2c5bc5c553f8ca999d17baf7be4f9e
alexzk1 <alexzkhr@gmail.com>
2018-07-14 03:21:30 +0300

    tryin another bad kernel

--- cl/kernels.cpp
+++ cl/kernels.cpp
@@ -277,21 +277,37 @@ cl::Program getCompiledKernels()
                   int16 atest = 0;
 
                   atest =  islessequal(fabs(angle - pi2), pi8); //90 not sure why, but this works better 90 = up/left
-                  p1 = myselectf16(p1, Z4, atest);
-                  p2 = myselectf16(p2, Z6, atest);
+//                  p1 = myselectf16(p1, Z4, atest);
+//                  p2 = myselectf16(p2, Z6, atest);
 
-                  atest =  isless(fabs(angle - pi4), pi8); //45
-                  p1 = myselectf16(p1, Z3, atest);
-                  p2 = myselectf16(p2, Z7, atest);
+//                  atest =  isless(fabs(angle - pi4), pi8); //45
+//                  p1 = myselectf16(p1, Z3, atest);
+//                  p2 = myselectf16(p2, Z7, atest);
 
-                  atest =  islessequal(angle, pi8) || islessequal(fabs(angle - pi1), pi8); //0
-                  p1 = myselectf16(p1, Z2, atest);
-                  p2 = myselectf16(p2, Z8, atest);
+//                  atest =  islessequal(angle, pi8) || islessequal(fabs(angle - pi1), pi8); //0
+//                  p1 = myselectf16(p1, Z2, atest);
+//                  p2 = myselectf16(p2, Z8, atest);
 
 
-                  atest =  isless(fabs(angle - pi34), pi8); //135
-                  p1 = myselectf16(p1, Z1, atest);
-                  p2 = myselectf16(p2, Z9, atest);
+//                  atest =  isless(fabs(angle - pi34), pi8); //135
+//                  p1 = myselectf16(p1, Z1, atest);
+//                  p2 = myselectf16(p2, Z9, atest);
+        p1 = myselectf16(p1, p2, atest);
+        p2 = myselectf16(p2, p1, atest);
+
+        atest =  isless(fabs(angle - pi4), pi8); //45
+        p1 = myselectf16(p1, p2, atest);
+        p2 = myselectf16(p2, p1, atest);
+
+        atest =  islessequal(angle, pi8) || islessequal(fabs(angle - pi1), pi8); //0
+        p1 = myselectf16(p1, p2, atest);
+        p2 = myselectf16(p2, p1, atest);
+
+
+        atest =  isless(fabs(angle - pi34), pi8); //135
+        p1 = myselectf16(p1, p2, atest);
+        p2 = myselectf16(p2, p1, atest);
+
 
                   //vstore16(Z5, 0, ( __global float*)(N + dstPaddedIndex));//DELETE IT, UNCOMMENT BELOW (it just copies magnitude from Sobel to output - for testing)
                   vstore16(myselectf16(0, Z5, isless(p2, Z5) && isless(p1, Z5)), 0, ( __global float*)(N + dstPaddedIndex));
